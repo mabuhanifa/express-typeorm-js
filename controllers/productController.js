876b0b0800ed217a78de2bf4fb41e79ca369ec4b
mabuhanifa <moh.abuhanifa@gmail.com>
2024-04-30 19:22:19 +0600

    getSingleProduct by id

--- controllers/productController.js
+++ controllers/productController.js
@@ -70,8 +70,30 @@ const updateProductStatus = async (req, res) => {
   }
 };
 
+const getSingleProduct = async (req, res) => {
+  try {
+    const productId = req.params.productId;
+
+    const product = await dataSource
+      .getRepository(Product)
+      .createQueryBuilder("product")
+      //   .leftJoinAndSelect("product.createdBy", "user")
+      .where("product.id = :productId", { productId: productId })
+      .getOne();
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.json({ product });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   getAllProducts,
   createProduct,
   updateProductStatus,
+  getSingleProduct,
 };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,10 +3,13 @@ const {
   getAllProducts,
   createProduct,
   updateProductStatus,
+  getSingleProduct,
 } = require("../controllers/productController");
 const router = Router();
 
 router.route("/").get(getAllProducts).post(createProduct);
+
+router.route("/:productId").get(getSingleProduct);
 
 router.route("/status").post(updateProductStatus);
 
